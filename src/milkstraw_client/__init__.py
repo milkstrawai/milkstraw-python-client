121ccb1015dc8fd9ee100120d9b14c8d93676bd2
Ibraheem Tuffaha <ibraheem.z.tuffaha@gmail.com>
2023-05-20 12:29:41 +0300

    Add APIClient implementation

--- src/milkstraw_client/__init__.py
+++ src/milkstraw_client/__init__.py
@@ -1,1 +1,9 @@
+import os
+
+from milkstraw_client.api_client import APIClient
 from milkstraw_client.test_api import test_url
+
+edge_service_url = os.environ.get("MILKSTRAW_EDGE_SERVICE_URL")
+
+user_email = os.environ.get("MILKSTRAW_USER_EMAIL")
+user_password = os.environ.get("MILKSTRAW_USER_PASS")
